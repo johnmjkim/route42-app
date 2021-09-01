b6d40c0189dd9d48f7cfaa467a8409cc2bde28ab
Kai Hirota <kaiyhirota@gmail.com>
2021-09-01 22:13:24 +1000

    minor change

--- app/src/main/java/com/comp6442/groupproject/ui/LogInActivity.java
+++ app/src/main/java/com/comp6442/groupproject/ui/LogInActivity.java
@@ -25,10 +25,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 
 import com.comp6442.groupproject.R;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
