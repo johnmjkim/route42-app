c0000d8bd9a6fe61ad3e7513ebc56d048fdc772a
Kai Hirota <kaiyhirota@gmail.com>
2021-09-17 22:26:14 +1000

    quick fix

--- app/src/main/java/com/comp6442/route42/ui/fragment/ProfileFragment.java
+++ app/src/main/java/com/comp6442/route42/ui/fragment/ProfileFragment.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
