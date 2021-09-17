0a4e8c61632ab772292416b0b4e69accdbd7db86
Kai Hirota <kaiyhirota@gmail.com>
2021-09-18 05:31:25 +1000

    save progress

--- app/src/main/java/com/comp6442/route42/ui/fragment/MapFragment.java
+++ app/src/main/java/com/comp6442/route42/ui/fragment/MapFragment.java
@@ -50,119 +50,119 @@ public class MapFragment extends Fragment {
   private Random random = new Random();
   private List<DocumentSnapshot> matchingDocs = new ArrayList<>();
 
-  private FusedLocationProviderClient fusedLocationClient;
-
-  public MapFragment() {
-    // Required empty public constructor
-  }
-
-  @SuppressLint("MissingPermission")
-  @Override
-  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                           Bundle savedInstanceState) {
-    setHasOptionsMenu(true);
-    mapbox = Mapbox.getInstance(requireContext(), getString(R.string.mapbox_api_key));
-
-    //    fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
-//    fusedLocationClient.getLastLocation().addOnSuccessListener(
-//            location -> userLocation = new LatLng(location.getLatitude(), location.getLongitude())
-//    );
-    userLocation = new LatLng(-32.7125008995674, 151.52910736650574);
-
-    return inflater.inflate(R.layout.fragment_map, container, false);
-  }
-
-  @Override
-  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
-
-    if (userLocation != null) {
-      Timber.i("Creating map. User location: (lat, lon) = %f, %f", userLocation.getLatitude(), userLocation.getLongitude());
-
-      final GeoLocation center = new GeoLocation(userLocation.getLatitude(), userLocation.getLongitude());
-      final double radiusInM = 5000 * 1000;
-
-      final List<Task<QuerySnapshot>> tasks = PostRepository.getInstance().getPostsWithinRadius(center, radiusInM);
-
-      // Collect all the query results together into a single list
-      Tasks.whenAllComplete(tasks)
-              .addOnCompleteListener(t -> {
-                for (Task<QuerySnapshot> task : tasks) {
-                  QuerySnapshot snap = task.getResult();
-                  for (DocumentSnapshot doc : snap.getDocuments()) {
-                    double lat = doc.getDouble("latitude");
-                    double lng = doc.getDouble("longitude");
-
-                    // We have to filter out a few false positives due to GeoHash
-                    // accuracy, but most will match
-                    GeoLocation docLocation = new GeoLocation(lat, lng);
-                    double distanceInM = GeoFireUtils.getDistanceBetween(docLocation, center);
-                    if (distanceInM <= radiusInM) {
-                      matchingDocs.add(doc);
-                    }
-                  }
-                }
-
-                // matchingDocs contains the results
-                Timber.i("Found %d documents within %f", matchingDocs.size(), radiusInM);
-                mMapView = view.findViewById(R.id.mapView);
-                mMapView.onCreate(savedInstanceState);
-                mMapView.getMapAsync(mapboxMap -> mapboxMap.setStyle(Style.DARK,
-                        style -> {
-                          mapboxMap.getUiSettings().setZoomGesturesEnabled(true);
-
-                          if (style.isFullyLoaded()) {
-                            circleManager = new CircleManager(mMapView, mapboxMap, style);
-
-                            circleManager.addClickListener(point -> {
-                              Toast.makeText(getActivity(),
-                                      String.format("Line clicked %s", point.getId()),
-                                      Toast.LENGTH_SHORT
-                              ).show();
-                              return false;
-                            });
-
-                            circleManager.addLongClickListener(point -> {
-                              Toast.makeText(getActivity(),
-                                      String.format("Line long clicked %s", point.getId()),
-                                      Toast.LENGTH_SHORT
-                              ).show();
-                              return false;
-                            });
-
-                            if (matchingDocs != null && matchingDocs.size() > 0) {
-//                              List<LatLng> points = new ArrayList<>();
-                              List<CircleOptions> circleOptionsList = new ArrayList<>();
-
-                              for (DocumentSnapshot matchingDoc : matchingDocs) {
-                                if (matchingDoc.contains("latitude") && matchingDocs.contains("longitude")){
-                                  LatLng point = new LatLng(
-                                          matchingDoc.getDouble("latitude"),
-                                          matchingDoc.getDouble("longitude")
-                                  );
-//                                  points.add(point);
-                                  
-                                  CircleOptions options = new CircleOptions()
-                                          .withLatLng(new LatLng(point.getLatitude(), point.getLongitude()))
-                                          .withCircleColor(ColorUtils.colorToRgbaString(Color.rgb(0, 255, 204)))
-                                          .withCircleRadius(20f)
-                                          .withDraggable(false);
-                                  circleOptionsList.add(options);
-                                }
-                              }
-
-                              circleManager.create(circleOptionsList);
-                              mapboxMap.setCameraPosition(new CameraPosition.Builder()
-                                      .target(userLocation)
-                                      // .zoom(calculateDistance(points.get(0), points.get(points.size() - 1)))
-                                      .build());
-                            }
-                          }
-                        }));
-              });
-
-    }
-  }
+//  private FusedLocationProviderClient fusedLocationClient;
+//
+//  public MapFragment() {
+//    // Required empty public constructor
+//  }
+//
+//  @SuppressLint("MissingPermission")
+//  @Override
+//  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+//                           Bundle savedInstanceState) {
+//    setHasOptionsMenu(true);
+//    mapbox = Mapbox.getInstance(requireContext(), getString(R.string.mapbox_api_key));
+//
+//    //    fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+////    fusedLocationClient.getLastLocation().addOnSuccessListener(
+////            location -> userLocation = new LatLng(location.getLatitude(), location.getLongitude())
+////    );
+//    userLocation = new LatLng(-32.7125008995674, 151.52910736650574);
+//
+//    return inflater.inflate(R.layout.fragment_map, container, false);
+//  }
+//
+//  @Override
+//  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//    super.onViewCreated(view, savedInstanceState);
+//
+//    if (userLocation != null) {
+//      Timber.i("Creating map. User location: (lat, lon) = %f, %f", userLocation.getLatitude(), userLocation.getLongitude());
+//
+//      final GeoLocation center = new GeoLocation(userLocation.getLatitude(), userLocation.getLongitude());
+//      final double radiusInM = 5000 * 1000;
+//
+//      final List<Task<QuerySnapshot>> tasks = PostRepository.getInstance().getPostsWithinRadius(center, radiusInM);
+//
+//      // Collect all the query results together into a single list
+//      Tasks.whenAllComplete(tasks)
+//              .addOnCompleteListener(t -> {
+//                for (Task<QuerySnapshot> task : tasks) {
+//                  QuerySnapshot snap = task.getResult();
+//                  for (DocumentSnapshot doc : snap.getDocuments()) {
+//                    double lat = doc.getDouble("latitude");
+//                    double lng = doc.getDouble("longitude");
+//
+//                    // We have to filter out a few false positives due to GeoHash
+//                    // accuracy, but most will match
+//                    GeoLocation docLocation = new GeoLocation(lat, lng);
+//                    double distanceInM = GeoFireUtils.getDistanceBetween(docLocation, center);
+//                    if (distanceInM <= radiusInM) {
+//                      matchingDocs.add(doc);
+//                    }
+//                  }
+//                }
+//
+//                // matchingDocs contains the results
+//                Timber.i("Found %d documents within %f", matchingDocs.size(), radiusInM);
+//                mMapView = view.findViewById(R.id.mapView);
+//                mMapView.onCreate(savedInstanceState);
+//                mMapView.getMapAsync(mapboxMap -> mapboxMap.setStyle(Style.DARK,
+//                        style -> {
+//                          mapboxMap.getUiSettings().setZoomGesturesEnabled(true);
+//
+//                          if (style.isFullyLoaded()) {
+//                            circleManager = new CircleManager(mMapView, mapboxMap, style);
+//
+//                            circleManager.addClickListener(point -> {
+//                              Toast.makeText(getActivity(),
+//                                      String.format("Line clicked %s", point.getId()),
+//                                      Toast.LENGTH_SHORT
+//                              ).show();
+//                              return false;
+//                            });
+//
+//                            circleManager.addLongClickListener(point -> {
+//                              Toast.makeText(getActivity(),
+//                                      String.format("Line long clicked %s", point.getId()),
+//                                      Toast.LENGTH_SHORT
+//                              ).show();
+//                              return false;
+//                            });
+//
+//                            if (matchingDocs != null && matchingDocs.size() > 0) {
+////                              List<LatLng> points = new ArrayList<>();
+//                              List<CircleOptions> circleOptionsList = new ArrayList<>();
+//
+//                              for (DocumentSnapshot matchingDoc : matchingDocs) {
+//                                if (matchingDoc.contains("latitude") && matchingDocs.contains("longitude")){
+//                                  LatLng point = new LatLng(
+//                                          matchingDoc.getDouble("latitude"),
+//                                          matchingDoc.getDouble("longitude")
+//                                  );
+////                                  points.add(point);
+//
+//                                  CircleOptions options = new CircleOptions()
+//                                          .withLatLng(new LatLng(point.getLatitude(), point.getLongitude()))
+//                                          .withCircleColor(ColorUtils.colorToRgbaString(Color.rgb(0, 255, 204)))
+//                                          .withCircleRadius(20f)
+//                                          .withDraggable(false);
+//                                  circleOptionsList.add(options);
+//                                }
+//                              }
+//
+//                              circleManager.create(circleOptionsList);
+//                              mapboxMap.setCameraPosition(new CameraPosition.Builder()
+//                                      .target(userLocation)
+//                                      // .zoom(calculateDistance(points.get(0), points.get(points.size() - 1)))
+//                                      .build());
+//                            }
+//                          }
+//                        }));
+//              });
+//
+//    }
+//  }
 
   private int calculateDistance(LatLng source, LatLng destination) {
     double distance = source.distanceTo(destination);
