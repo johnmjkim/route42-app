7d78233fbe8ee8f7d5bf4a845aab23f60441063c
honggic <cjswoghdwlr@gmail.com>
2021-10-14 16:19:55 +0900

    Upload basic test

---
+++ app/src/test/java/com/comp6442/route42/apiclient/QueryStringTest.java
@@ -0,0 +1,21 @@
+package com.comp6442.route42.apiclient;
+import com.comp6442.route42.api.QueryString;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class QueryStringTest {
+    String query;
+    String parameter1 = "field";
+    String parameter2 = "type";
+    String value1 = "[@field:fieldName]";
+    String value2 = "[@Type:type]";
+    char symbol = '?';
+    QueryString querySample = new QueryString(query);
+    @Test
+    public void checkQuery() {
+        querySample.setQuery(symbol+parameter1+"="+value1+"&"+parameter2+"="+value2);
+        Assert.assertEquals(querySample.getQuery(),"?field=[@field:fieldName]&type=[@Type:type]");
+    }
+}
