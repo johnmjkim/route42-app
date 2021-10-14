611398064ff7098454a5619eb6a81c31494a2a25
Min Jae Kim <u7269158@anu.edu.au>
2021-10-14 20:06:35 +0900

    MockLocationTest Created
    Signed-off-by: Min Jae Kim <u7269158@anu.edu.au>

---
+++ app/src/test/java/com/comp6442/route42/model/MockLocationTest.java
@@ -0,0 +1,20 @@
+package com.comp6442.route42.model;
+
+import com.comp6442.route42.data.model.Activity;
+import com.comp6442.route42.data.model.MockLocation;
+import com.google.android.gms.maps.model.LatLng;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.time.*;
+
+import timber.log.Timber;
+
+public class MockLocationTest {
+
+
+}
