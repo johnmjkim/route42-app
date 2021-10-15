f90d894ea092f82ec33938b71855c8de19b6c939
Min Jae Kim <u7269158@anu.edu.au>
2021-10-16 00:34:53 +0900

    kNearestNeighbourService Test created
    Signed-off-by: Min Jae Kim <u7269158@anu.edu.au>

---
+++ app/src/test/java/com/comp6442/route42/api/KNearestNeighbourServiceTest.java
@@ -0,0 +1,24 @@
+package com.comp6442.route42.api;
+
+import com.comp6442.route42.data.model.Post;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import timber.log.Timber;
+
+public class KNearestNeighbourServiceTest {
+    @Test
+    public void test1(){
+        KNearestNeighbourService kNearestNeighbourService = new KNearestNeighbourService(5, -35.25932077515105, 149.11459641897002);
+        Timber.i(String.valueOf(kNearestNeighbourService));
+        try {
+            for(Post post : kNearestNeighbourService.call()){
+                Timber.i(String.valueOf(post));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
