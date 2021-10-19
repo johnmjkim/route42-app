aa4f90f97aa9f65a3c8f4cc72fafc0465316e66b
Min Jae Kim <u7269158@anu.edu.au>
2021-10-20 00:20:05 +0900

    KNearestNeighbourServiceTest complete
    Signed-off-by: Min Jae Kim <u7269158@anu.edu.au>

--- app/src/test/java/com/comp6442/route42/apiclient/KNearestNeighbourServiceTest.java
+++ app/src/test/java/com/comp6442/route42/apiclient/KNearestNeighbourServiceTest.java
@@ -3,6 +3,7 @@ package com.comp6442.route42.apiclient;
 import com.comp6442.route42.api.KNearestNeighbourService;
 import com.comp6442.route42.data.model.Post;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
@@ -10,16 +11,18 @@ import java.util.List;
 import timber.log.Timber;
 
 public class KNearestNeighbourServiceTest {
+    int k = 5;
+    Double lat = -35.25932077515105;
+    Double lon = 149.11459641897002;
+    KNearestNeighbourService kNearestNeighbourService = new KNearestNeighbourService(k, lat, lon);
+
     @Test
-    public void test1(){
-        KNearestNeighbourService kNearestNeighbourService = new KNearestNeighbourService(5, -35.25932077515105, 149.11459641897002);
-        Timber.i(String.valueOf(kNearestNeighbourService));
-        try {
-            for(Post post : kNearestNeighbourService.call()){
-                Timber.i(String.valueOf(post));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void callTest() throws Exception {
+        Assert.assertEquals(null, kNearestNeighbourService.call());
+    }
+
+    @Test
+    public void toStringTest() {
+        Assert.assertEquals("KNearestNeighbourService{k=5, lat=-35.25932077515105, lon=149.11459641897002}", kNearestNeighbourService.toString());
     }
 }
--- app/src/test/java/com/comp6442/route42/model/UserViewModelTest.java
+++ app/src/test/java/com/comp6442/route42/model/UserViewModelTest.java
@@ -1,11 +1,16 @@
 package com.comp6442.route42.model;
 
+import com.comp6442.route42.ui.viewmodel.UserViewModel;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Date;
 
 public class UserViewModelTest {
 
     @Test
     public void test1(){
-
+        UserViewModel userViewModel = new UserViewModel();
     }
 }
