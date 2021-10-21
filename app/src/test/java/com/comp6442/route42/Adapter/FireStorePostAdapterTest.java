2c23cd69bcca1f3599403de072c8f8fc898765a4
honggic <cjswoghdwlr@gmail.com>
2021-10-21 22:44:48 +0900

    Make FireStorePostAdapterTest(checkView)

---
+++ app/src/test/java/com/comp6442/route42/Adapter/FireStorePostAdapterTest.java
@@ -0,0 +1,18 @@
+package com.comp6442.route42.Adapter;
+
+import static org.mockito.Mockito.mock;
+import android.view.View;
+import com.comp6442.route42.ui.adapter.FirestorePostAdapter;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FireStorePostAdapterTest {
+
+    View view = mock(View.class);
+
+    @Test
+    public void checkView(){
+        FirestorePostAdapter.PostViewHolder postViewHolder = new FirestorePostAdapter.PostViewHolder(view);
+        Assert.assertNotNull(postViewHolder);
+    }
+}
